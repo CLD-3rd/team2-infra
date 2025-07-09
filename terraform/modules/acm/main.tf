66a21d7d778947d3dec054c5b9ee5e2f9f178c58
siiiirru <eqeee@naver.com>
2025-07-09 09:52:06 +0900

    infra:acm zone.id관련 조건문 삭제

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -1,4 +1,5 @@
 resource "aws_acm_certificate" "this" {
+  
   domain_name               = var.domain_name
   subject_alternative_names = var.subject_alternative_names
   validation_method         = var.validation_method
@@ -16,7 +17,7 @@ resource "aws_route53_record" "validation" {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
-    } if var.zone_id != ""
+    }
   }
 
   allow_overwrite = true
@@ -28,7 +29,7 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "this" {
-  count           = var.zone_id != "" ? 1 : 0
   certificate_arn = aws_acm_certificate.this.arn
   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+  depends_on = [ aws_route53_record.validation ]
 }
