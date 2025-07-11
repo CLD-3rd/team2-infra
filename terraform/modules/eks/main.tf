53f550ad8d7056a5190269269b540375ec2c0537
siiiirru <eqeee@naver.com>
2025-07-11 13:34:46 +0900

    fix:클러스터 보안그룹 수정

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -78,7 +78,7 @@ resource "aws_security_group" "cluster_sg" {
     from_port = 443
     to_port   = 443
     protocol  = "tcp"
-    cidr_blocks = ["10.22.0.0/16"]
+    cidr_blocks = ["10.22.0.0/16","192.168.0.0/16"]
   }
 
   egress {
